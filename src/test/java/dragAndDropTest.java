b6a0616dbc0347c8d1b7ed52748ec57c9662e082
azarsen1 <azarsen4@gmail.com>
2025-03-16 13:10:41 +0500

    addTests

--- src/test/java/dragAndDropTest.java
+++ src/test/java/dragAndDropTest.java
@@ -1,2 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class dragAndDropTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void dragAndDropTestWithAction() {
+        open("drag_and_drop");
+        $("#column-a").hover();
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    void dragAndDropTestWithoutAction() {
+        open("drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-b").shouldHave(text("A"));
+    }
 }
